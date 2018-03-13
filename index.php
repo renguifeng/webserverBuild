bbbc80ab782a44bee987a5ef22eb24903afcf0a5
renguifeng <guifeng1111@qq.com>
2018-03-13 12:58:59 +0800

    1

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-
+error_reporting(E_ALL);
 include_once("mysql.php");
 
 $cfg = parse_ini_file("/opt/config.ini");
-
+var_dump($cfg);
 $db = new mysql($cfg['host'],$cfg['user'],$cfg['pwd'],$cfg['db']);
 
 var_dump($db);
