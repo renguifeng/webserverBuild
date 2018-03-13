3db5bf306242a3767cdfe5113ef98692dbf7a528
renguifeng <guifeng1111@qq.com>
2018-03-13 13:01:24 +0800

    1

--- index.php
+++ index.php
@@ -6,4 +6,7 @@ $cfg = parse_ini_file("/opt/config.ini");
 var_dump($cfg);
 $db = new mysql($cfg['host'],$cfg['user'],$cfg['pwd'],$cfg['db']);
 
-var_dump($db);
+$sql = "select * from web_user";
+$db->query($sql);
+$row = $db->fetchAll();
+var_dump($row);
